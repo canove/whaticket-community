5a1b79ea7e9416fbafe6ace0e22d62e5d61641cb
canove <cassio@economicros.com.br>
2020-07-28 16:07:52 -0300

    feat: Added favicon and changed app name

--- frontend/src/components/SessionInfo/index.js
+++ frontend/src/components/SessionInfo/index.js
@@ -1,17 +1,8 @@
 import React from "react";
-import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles({
-	main: {
-		// flex: 1,
-	},
-});
 
 const SessionInfo = ({ session }) => {
 	console.log(session);
-	const classes = useStyles();
 	return (
 		<div>
 			<Typography component="h2" variant="h6" color="primary" gutterBottom>
