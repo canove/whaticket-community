a879bb95caa0dbebe849c4ce4795b37f8e69d747
canove <cassio@economicros.com.br>
2020-09-20 11:40:18 -0300

    fix: no extra info in contacts

--- backend/src/services/ContactServices/ShowContactService.ts
+++ backend/src/services/ContactServices/ShowContactService.ts
@@ -2,7 +2,7 @@ import Contact from "../../models/Contact";
 import AppError from "../../errors/AppError";
 
 const ShowContactService = async (id: string | number): Promise<Contact> => {
-  const user = await Contact.findByPk(id);
+  const user = await Contact.findByPk(id, { include: ["extraInfo"] });
 
   if (!user) {
     throw new AppError("No contact found with this ID.", 404);
