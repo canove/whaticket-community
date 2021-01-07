3b80723f7a7e731a20aeab763f7de347907e8c48
Vinícius <18627611+esteves67@users.noreply.github.com>
2021-01-07 06:49:23 -0300

    Adds Webhook API - Issue #17
    Resolves #17

--- backend/src/services/WbotServices/wbotMessageListener.ts
+++ backend/src/services/WbotServices/wbotMessageListener.ts
@@ -21,6 +21,17 @@ import AppError from "../../errors/AppError";
 import ShowTicketService from "../TicketServices/ShowTicketService";
 import CreateMessageService from "../MessageServices/CreateMessageService";
 
+/* API WEBHOOK */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const axios = require('axios');
+const mime = require('mime-types');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+/* API WEBHOOK */
+
 interface Session extends Client {
   id?: number;
 }
@@ -351,6 +362,117 @@ const wbotMessageListener = (wbot: Session): void => {
   wbot.on("message_ack", async (msg, ack) => {
     handleMsgAck(msg, ack);
   });
+  
+  /* API WEBHOOK */
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+app.post('/send', async (req, res)=>{
+
+  let num = req.body.num;
+  let msg = req.body.message;
+
+  let numbers = Array();
+  num.split(',').map((number)=>{ 
+    numbers.push(number.trim()) 
+  });  
+
+
+  try{
+
+      const success = Array();
+      for(let i=0; i<numbers.length; i++){
+
+        let number = numbers[i]+'@c.us'; 
+        const data = await wbot.sendMessage(number, msg);
+
+        if(data.ack === 0){
+          success.push({success: true, number: numbers[i], detail: numbers[i] + ", Message status success"});
+        }else{
+          success.push({success: false, number: numbers[i], detail: numbers[i] + ", Message status failed"});
+        }
+
+      } 
+
+      res.json({status: success})
+
+
+  }catch(err){
+
+    res.json({status: "error", detail: "Failed to send message, Please authenticate or scan generated barcode"});
+
+  }
+
+})
+
+app.post('/sendMedia', async (req, res)=>{
+
+  let fileUrl = req.body.file;
+  let caption = req.body.message;
+  let num = req.body.num; 
+
+  let numbers = Array();
+  num.split(',').map((number)=>{ 
+    numbers.push(number.trim()) 
+  }); 
+
+
+
+  try{
+
+    let request = await axios.get(fileUrl, {responseType: 'stream'});
+
+    let cType = request.headers['content-type'];
+    const fileExt = mime.extension(cType);
+
+    let code = makeid(10).toUpperCase();
+    const dirLoc = './public/'+code
+    fs.mkdirSync(dirLoc);
+    const fname = makeid(10).toUpperCase()+"."+fileExt;
+
+
+    const dataUpload = request.data.pipe(fs.createWriteStream(dirLoc+'/'+fname));
+    dataUpload.on('finish', async ()=>{
+
+      const media = MessageMedia.fromFilePath(dirLoc+"/"+fname); 
+      const success = Array();
+
+      for(let i=0; i<numbers.length; i++){
+        const data = await wbot.sendMessage(numbers[i]+'@c.us', media, {caption: caption}); 
+        if(data.ack === 0){
+          success.push({success: true, number: numbers[i], detail: numbers[i] + ", Message status success"});
+        }else{
+          success.push({success: false, number: numbers[i], detail: numbers[i] + ", Message status failed"});
+        }
+      }
+
+
+      fs.unlinkSync(dirLoc+"/"+fname);
+      fs.rmdirSync(dirLoc);
+
+      res.json({status: success});
+      res.end();
+
+    })
+
+  }catch(err){
+
+    res.json({status: "error", details: "failed to check the URL!"});
+
+  }
+
+
+});
+
+app.listen('3030');
+/* API WEBHOOK */
 };
 
 export { wbotMessageListener, handleMessage };
