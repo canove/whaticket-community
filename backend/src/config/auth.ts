1a6b8b2e6a7656baecf099fe1baa86cb5f5895f7
canove <cassio@economicros.com.br>
2021-01-15 07:25:51 -0300

    fix: default token expiration time

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret: process.env.JWT_SECRET || "mysecret",
-  expiresIn: "15d",
+  expiresIn: "15m",
   refreshSecret: process.env.JWT_REFRESH_SECRET || "myanothersecret",
   refreshExpiresIn: "7d"
 };
