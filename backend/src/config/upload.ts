584b962d1d9f677cf2154028f826d2ad0c758231
Vinícius <18627611+esteves67@users.noreply.github.com>
2021-01-17 03:16:29 -0300

    Send file starting with filename
    This commit makes so that files send in the frontend starts with filename and then after the epoch timestamp, e.g: invoice-1610863629200.pdf ,
    so the person that receives the file can identify what's the file about instead of receiving a file with just numbers e.g: 1610863629200.pdf .

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -8,7 +8,8 @@ export default {
   storage: multer.diskStorage({
     destination: publicFolder,
     filename(req, file, cb) {
-      const fileName = new Date().getTime() + path.extname(file.originalname);
+      var arquivo = file.originalname;
+      const fileName = arquivo.substring(0, arquivo.lastIndexOf(".")) + '-' + new Date().getTime() + path.extname(file.originalname);
 
       return cb(null, fileName);
     }
