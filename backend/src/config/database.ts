0b371ab44562119e8dd375f17fd9a5a47361e8a8
Alan Martines <alancpmartines@hotmail.com>
2021-09-17 10:36:27 -0400

    Add port db option
    Add port db option

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -8,6 +8,7 @@ module.exports = {
   dialect: process.env.DB_DIALECT || "mysql",
   timezone: "-03:00",
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
