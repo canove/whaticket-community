7cce185cd86d8e19a1440cedc3d2e4e473ff19b4
dionatas <dionataslazzari@outlook.com.br>
2021-10-30 17:40:13 -0300

    fix change time to 1 day

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret: process.env.JWT_SECRET || "mysecret",
-  expiresIn: "15m",
+  expiresIn: "1d",
   refreshSecret: process.env.JWT_REFRESH_SECRET || "myanothersecret",
   refreshExpiresIn: "7d"
 };
