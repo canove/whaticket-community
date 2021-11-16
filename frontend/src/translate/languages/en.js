766af73fa7f8fc5957211c5d5500c952d953154c
Patricio Silva <62783690+ertprs@users.noreply.github.com>
2021-11-16 19:44:10 -0300

    Update en.js

--- frontend/src/translate/languages/en.js
+++ frontend/src/translate/languages/en.js
@@ -236,6 +236,8 @@ const messages = {
       transferTicketModal: {
         title: "Transfer Ticket",
         fieldLabel: "Type to search for users",
+        fieldQueueLabel: "Transfer to queue",
+        fieldQueuePlaceholder: "Please select a queue",
         noOptions: "No user found with this name",
         buttons: {
           ok: "Transfer",
@@ -366,7 +368,7 @@ const messages = {
         },
       },
       messagesInput: {
-        placeholderOpen: "Type a message",
+        placeholderOpen: "Type a message or press ''/'' to use the registered quick responses",
         placeholderClosed: "Reopen or accept this ticket to send a message.",
         signMessage: "Sign",
       },
