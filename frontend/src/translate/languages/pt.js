5d1d99967e526391c3c26efc0abf2d09c3d103f3
Patricio Silva <62783690+ertprs@users.noreply.github.com>
2021-11-16 19:38:22 -0300

    Update pt.js

--- frontend/src/translate/languages/pt.js
+++ frontend/src/translate/languages/pt.js
@@ -238,6 +238,8 @@ const messages = {
       transferTicketModal: {
         title: "Transferir Ticket",
         fieldLabel: "Digite para buscar usuários",
+        fieldQueueLabel: "Transferir para fila",
+        fieldQueuePlaceholder: "Selecione uma fila",
         noOptions: "Nenhum usuário encontrado com esse nome",
         buttons: {
           ok: "Transferir",
@@ -370,7 +372,7 @@ const messages = {
         },
       },
       messagesInput: {
-        placeholderOpen: "Digite uma mensagem",
+        placeholderOpen: "Digite uma mensagem ou tecle ''/'' para utilizar as respostas rápidas cadastrada",
         placeholderClosed:
           "Reabra ou aceite esse ticket para enviar uma mensagem.",
         signMessage: "Assinar",
