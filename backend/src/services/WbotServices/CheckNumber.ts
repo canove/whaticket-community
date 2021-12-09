613e89b64e586919d753efbf5d93fc25a94e8220
Filipe Borelli <60223308+filipeborelli@users.noreply.github.com>
2021-12-09 17:05:34 -0300

    Create CheckNumber.ts
    Create CheckNumber to get oficial number with 9 and without 9 for brasillian.

---
+++ backend/src/services/WbotServices/CheckNumber.ts
@@ -0,0 +1,13 @@
+import GetDefaultWhatsApp from "../../helpers/GetDefaultWhatsApp";
+import { getWbot } from "../../libs/wbot";
+
+const CheckContactNumber = async (number: string): Promise<void> => {
+  const defaultWhatsapp = await GetDefaultWhatsApp();
+
+  const wbot = getWbot(defaultWhatsapp.id);
+
+    const validNumber : any = await wbot.getNumberId(`${number}@c.us`);
+    return validNumber.user
+};
+
+export default CheckContactNumber;
