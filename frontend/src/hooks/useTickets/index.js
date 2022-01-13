31bce333da1477429b647d8a066069c2afb53572
Danilo Alves <danilo.tecvip@gmail.com>
2022-01-13 14:47:01 -0300

    Corrigido problema na contagem de chamados e adicionado possibilidade de fechar chamados automaticamente após um periodo de tempo

--- frontend/src/hooks/useTickets/index.js
+++ frontend/src/hooks/useTickets/index.js
@@ -4,56 +4,84 @@ import toastError from "../../errors/toastError";
 import api from "../../services/api";
 
 const useTickets = ({
-	searchParam,
-	pageNumber,
-	status,
-	date,
-	showAll,
-	queueIds,
-	withUnreadMessages,
+    searchParam,
+    pageNumber,
+    status,
+    date,
+    showAll,
+    queueIds,
+    withUnreadMessages,
 }) => {
-	const [loading, setLoading] = useState(true);
-	const [hasMore, setHasMore] = useState(false);
-	const [tickets, setTickets] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [hasMore, setHasMore] = useState(false);
+    const [tickets, setTickets] = useState([]);
+    const [count, setCount] = useState(0);
 
-	useEffect(() => {
-		setLoading(true);
-		const delayDebounceFn = setTimeout(() => {
-			const fetchTickets = async () => {
-				try {
-					const { data } = await api.get("/tickets", {
-						params: {
-							searchParam,
-							pageNumber,
-							status,
-							date,
-							showAll,
-							queueIds,
-							withUnreadMessages,
-						},
-					});
-					setTickets(data.tickets);
-					setHasMore(data.hasMore);
-					setLoading(false);
-				} catch (err) {
-					setLoading(false);
-					toastError(err);
-				}
-			};
-			fetchTickets();
-		}, 500);
-		return () => clearTimeout(delayDebounceFn);
-	}, [
-		searchParam,
-		pageNumber,
-		status,
-		date,
-		showAll,
-		queueIds,
-		withUnreadMessages,
-	]);
+    useEffect(() => {
+        setLoading(true);
+        const delayDebounceFn = setTimeout(() => {
+            const fetchTickets = async() => {
+                try {
+                    const { data } = await api.get("/tickets", {
+                        params: {
+                            searchParam,
+                            pageNumber,
+                            status,
+                            date,
+                            showAll,
+                            queueIds,
+                            withUnreadMessages,
+                        },
+                    })
+                    setTickets(data.tickets)
 
-	return { tickets, loading, hasMore };
+                    let horasFecharAutomaticamente = process.env.REACT_APP_HOURS_CLOSE_TICKETS_AUTO
+
+                    if (status === "open" && horasFecharAutomaticamente && horasFecharAutomaticamente !== "" &&
+                        horasFecharAutomaticamente !== "0" && Number(horasFecharAutomaticamente) > 0) {
+
+                        let dataLimite = new Date()
+                        dataLimite.setHours(dataLimite.getHours() - Number(horasFecharAutomaticamente))
+
+                        data.tickets.forEach(ticket => {
+                            if (ticket.status !== "closed") {
+                                let dataUltimaInteracaoChamado = new Date(ticket.updatedAt)
+                                if (dataUltimaInteracaoChamado < dataLimite)
+                                    closeTicket(ticket)
+                            }
+                        })
+                    }
+
+                    setHasMore(data.hasMore)
+                    setCount(data.count)
+                    setLoading(false)
+                } catch (err) {
+                    setLoading(false)
+                    toastError(err)
+                }
+            }
+
+            const closeTicket = async(ticket) => {
+                await api.put(`/tickets/${ticket.id}`, {
+                    status: "closed",
+                    userId: ticket.userId || null,
+                })
+            }
+
+            fetchTickets()
+        }, 500)
+        return () => clearTimeout(delayDebounceFn)
+    }, [
+        searchParam,
+        pageNumber,
+        status,
+        date,
+        showAll,
+        queueIds,
+        withUnreadMessages,
+    ])
+
+    return { tickets, loading, hasMore, count };
 };
 
 export default useTickets;
