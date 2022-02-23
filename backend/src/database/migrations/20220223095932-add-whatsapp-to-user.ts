67ad73a8bdb1b6966ea343696ec9ce68f0a0ab86
Ricardo Paes <ricardo@likesistemas.com.br>
2022-02-23 11:06:31 -0300

    :sparkles: add user association with whatsapps

---
+++ backend/src/database/migrations/20220223095932-add-whatsapp-to-user.ts
@@ -0,0 +1,17 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.addColumn("Users", "whatsappId", {
+      type: DataTypes.INTEGER,
+      references: { model: "Whatsapps", key: "id" },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+      allowNull: true
+    },);
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.removeColumn("Users", "whatsappId");
+  }
+};
