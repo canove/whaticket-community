64482604369d156048dc24fbc60f2a83acefcfe6
alonsosimoes <45598711+alonsosimoes@users.noreply.github.com>
2022-03-30 11:07:36 -0400

    Change name

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -217,7 +217,7 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            WhaTicket
+            MultiWhats
           </Typography>
           {user.id && <NotificationsPopOver />}
 
