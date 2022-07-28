cff06d94beb7a686d447f25664157b600832f142
Jocowski <jocowski@outlook.com>
2022-07-28 10:27:30 -0300

    Remove unused import

--- frontend/src/pages/Reports/index.js
+++ frontend/src/pages/Reports/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
