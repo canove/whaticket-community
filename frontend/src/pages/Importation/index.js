409163bfb38284c22a33326d74cf7c6edc0041cf
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2022-08-04 11:00:22 -0300

    Update

--- frontend/src/pages/Importation/index.js
+++ frontend/src/pages/Importation/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -21,6 +21,7 @@ import ImportModal from "../../components/ImportModal";
 import { toast } from "react-toastify";
 import toastError from "../../errors/toastError";
 import api from "../../services/api";
+import { format, parseISO } from "date-fns";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,15 +36,60 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const reducer = (state, action) => {
+  if (action.type === "LOAD_USERS") {
+    const users = action.payload;
+    const newUsers = [];
+
+    users.forEach((user) => {
+      const userIndex = state.findIndex((u) => u.id === user.id);
+      if (userIndex !== -1) {
+        state[userIndex] = user;
+      } else {
+        newUsers.push(user);
+      }
+    });
+
+    return [...state, ...newUsers];
+  }
+
+  if (action.type === "RESET") {
+    return [];
+  }
+};
+
 const Importation = () => {
   const classes = useStyles();
   const { i18n } = useTranslation();
 
   const [loading, setLoading] = useState(false);
-  const [date, setDate] = useState();
-  const [status, setStatus] = useState();
+  const [date, setDate] = useState("");
+  const [status, setStatus] = useState("");
   const [imports, setImports] = useState([]);
   const [importModalOpen, setImportModalOpen] = useState(false);
+
+  const [users, dispatchUsers] = useReducer(reducer, []);
+
+  useEffect(() => {
+    dispatchUsers({ type: "RESET" });
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    const delayDebounceFn = setTimeout(() => {
+      const fetchUsers = async () => {
+        try {
+          const { data } = await api.get("/users/");
+          dispatchUsers({ type: "LOAD_USERS", payload: data.users });
+          setLoading(false);
+        } catch (err) {
+          toastError(err);
+        }
+      };
+      fetchUsers();
+    }, 500);
+    return () => clearTimeout(delayDebounceFn);
+  }, []);
 
   const renderOptionLabel = (option) => {
     return option;
@@ -57,8 +103,46 @@ const Importation = () => {
     setImportModalOpen(false);
   };
 
+  const getStatusByName = (status) => {
+    if (status === "Novo") {
+      return "0";
+    } else if (status === "Processando") {
+      return "1";
+    } else if (status === "Processado") {
+      return "3";
+    } else if (status === "Erro") {
+      return "4";
+    } else {
+      return status;
+    }
+  };
+
+  const getStatusById = (id) => {
+    if (id === 0) {
+      return "Novo";
+    } else if (id === 1) {
+      return "Processando";
+    } else if (id === 2) {
+      return "Processado";
+    } else if (id === 3) {
+      return "Erro";
+    } else {
+      return id;
+    }
+  };
+
+  const getUserById = (userId) => {
+    let response = "";
+    users.map((user) => {
+      if (user.id === userId) {
+        response = user.name;
+      }
+    });
+    return response;
+  };
+
   const handleSelectOption = (e, newValue) => {
-    setStatus(newValue);
+    setStatus(getStatusByName(newValue));
   };
 
   const handleFilter = async () => {
@@ -68,7 +152,9 @@ const Importation = () => {
     } else {
       try {
         setLoading(true);
-        const { data } = await api.get(`file/list?Status=${status}&initialDate=${date}`);
+        const { data } = await api.get(
+          `file/list?Status=${status}&initialDate=${date}`
+        );
         setImports(data);
         setLoading(false);
       } catch (err) {
@@ -89,7 +175,7 @@ const Importation = () => {
         <MainHeaderButtonsWrapper>
           <Autocomplete
             className={classes.root}
-            options={["0", "1"]}
+            options={["Novo", "Processando", "Processado", "Erro"]}
             getOptionLabel={renderOptionLabel}
             onChange={(e, newValue) => handleSelectOption(e, newValue)}
             renderInput={(params) => (
@@ -143,18 +229,24 @@ const Importation = () => {
           </TableHead>
           <TableBody>
             <>
-            {imports.map((item, index) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell align="center">{item.createdAt}</TableCell>
-                  <TableCell align="center">{item.name}</TableCell>
-                  <TableCell align="center">{item.ownerid}</TableCell>
-                  <TableCell align="center">{item.QtdeRegister}</TableCell>
-                  <TableCell align="center">{item.Status}</TableCell>
-                </TableRow>
-              );
-            })}
-            {loading}
+              {imports.map((item, index) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell align="center">
+                      {format(parseISO(item.createdAt), "dd/MM/yy HH:mm")}
+                    </TableCell>
+                    <TableCell align="center">{item.name}</TableCell>
+                    <TableCell align="center">
+                      {getUserById(item.ownerid)}
+                    </TableCell>
+                    <TableCell align="center">{item.QtdeRegister}</TableCell>
+                    <TableCell align="center">
+                      {getStatusById(item.Status)}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              {loading}
             </>
           </TableBody>
         </Table>
