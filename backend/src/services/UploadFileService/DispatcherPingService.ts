72b8b9d4113878421588a5fc493f158ccc242e99
Ricardo Andrade <ricardo.andrade@roit.com.br>
2022-08-29 15:17:34 -0300

    worker ping

--- backend/src/services/UploadFileService/DispatcherPingService.ts
+++ backend/src/services/UploadFileService/DispatcherPingService.ts
@@ -5,38 +5,19 @@ import randomWords from "random-words";
 import Whatsapp from "../../database/models/Whatsapp";
 
 /* eslint-disable */
-const DispatcherPingService = async ({ file }): Promise<void> => {
+const DispatcherPingService = async (): Promise<void> => {
   try {
-    let whatsappIds;
-    if (file.whatsappIds && file.whatsappIds != 'null')
-      whatsappIds = file.whatsappIds.split(",");
-
-    let accounts;
-
-    if(whatsappIds) {
-      accounts = await Whatsapp.findAll({
-        where: {
-          id: whatsappIds, 
-          official: false
-        },
-        limit: 2,
-        order: [['lastPingDate', 'ASC']],
-      });
-    }else{
-      accounts = await Whatsapp.findAll({
-        where: {
-          status: "CONNECTED",
-          [Op.or]: [
-            {
-              official: false
-            }
-          ]
-        },
-        limit: 2,
-        order: [['lastPingDate', 'ASC']],
-      });
-    }
-
+    let accounts = await Whatsapp.findAll({
+      where: {
+        status: "CONNECTED",
+        [Op.or]: [
+          {
+            official: false
+          }
+        ]
+      },
+      order: [['lastPingDate', 'ASC']],
+    });
     const apiUrl = `${process.env.WPP_OFFICIAL_URL}?x-api-key=${process.env.WPP_OFFICIAL_API_KEY}`;
     let provider;
 
@@ -46,31 +27,32 @@ const DispatcherPingService = async ({ file }): Promise<void> => {
     }
 
     if(provider) {
-      for (const account of accounts) {
+      accounts.forEach(async (account) => {
         if (!account.official && provider.name != account.name) { 
-              const payload = [{
-                company: '102780189204674', // QUANDO MUDAR PARA VARIAS EMPRESAS DEIXAR DINAMICO
-                person: account.name,
-                activationMessage: {
-                  session: provider.name,
-                  msgid: 0,
-                  channel: "wpp_no",
-                  template: "",
-                  to: {
-                    identifier: account.name,
-                    name: account.name
-                  },
-                  text: `${randomWords({ exactly: 4, join: ' ' })} -PING-`,
-                  parameters: []
-                }
-              }];
-  
-              await new Promise((resolve) => { setTimeout(() => resolve(true), Math.floor(Math.random() * 10000) + 3000) })
-              await axios.post(apiUrl, JSON.stringify(payload), { headers: {
-                "x-api-key": process.env.WPP_OFFICIAL_API_KEY
-              }});
-          }
+          const randomWordsNumber = Math.floor(Math.random() * 1) + 6;
+          const payload = [{
+            company: '102780189204674', // QUANDO MUDAR PARA VARIAS EMPRESAS DEIXAR DINAMICO
+            person: account.name,
+            activationMessage: {
+              session: provider.name,
+              msgid: 0,
+              channel: "wpp_no",
+              template: "",
+              to: {
+                identifier: account.name,
+                name: account.name
+              },
+              text: `${randomWords({ exactly: randomWordsNumber, join: ' ' })} -PING-`,
+              parameters: []
+            }
+          }];
+
+          await new Promise((resolve) => { setTimeout(() => resolve(true), Math.floor(Math.random() * 10000) + 3000) })
+          await axios.post(apiUrl, JSON.stringify(payload), { headers: {
+            "x-api-key": process.env.WPP_OFFICIAL_API_KEY
+          }});
       }
+      })
     }
   } catch (e) {
     console.log(e);
