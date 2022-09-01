a146fc98734f3bf75e9fce76cca367ab4ca3cebf
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2022-09-01 10:03:55 -0300

    Update Menus

--- backend/src/services/MenuServices/ShowCompanyMenuService.ts
+++ backend/src/services/MenuServices/ShowCompanyMenuService.ts
@@ -1,14 +1,13 @@
 import Company from "../../database/models/Company";
 import Menu from "../../database/models/Menu";
 
-const ListMenusService = async (): Promise<Menu[]> => {
+const ShowCompanyMenuService = async (id: string | number): Promise<Menu[]> => {
   const menus = await Menu.findAll({
-    attributes: ["id", "name", "icon", "isParent", "parentId"],
     include: [
       {
         model: Company,
         as: "companies",
-        attributes: ["id"]
+        where: { id }
       }
     ]
   });
@@ -16,4 +15,4 @@ const ListMenusService = async (): Promise<Menu[]> => {
   return menus;
 };
 
-export default ListMenusService;
+export default ShowCompanyMenuService;
