bd7436984c4bf580945bebe3549dcab64ead115a
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2022-09-21 15:46:42 -0300

    Dashboard Dont Show Rejected Files

--- backend/src/services/UploadFileService/UpdateFileService.ts
+++ backend/src/services/UploadFileService/UpdateFileService.ts
@@ -2,21 +2,21 @@ import { FileStatus } from "../../enum/FileStatus";
 import ShowFileService from "./ShowFileService";
 
 const UpdateFileService = async ({
-    status,
-    userId,
-    fileId
+  status,
+  userId,
+  fileId
 }): Promise<unknown> => {
   const file = await ShowFileService(fileId);
   const date = new Date();
 
-  if (status == '4') {
+  if (status == "4") {
     await file.update({ Status: FileStatus.WaitingDispatcher });
     await file.update({ approvedAt: date });
     await file.update({ approvedOrRefusedId: userId });
     return file;
   }
 
-  if (status == '7') {
+  if (status == "7") {
     await file.update({ Status: FileStatus.Refused });
     await file.update({ refusedAt: date });
     await file.update({ approvedOrRefusedId: userId });
@@ -25,6 +25,6 @@ const UpdateFileService = async ({
 
   await file.update({ Status: FileStatus.Error });
   return file;
-}
+};
 
 export default UpdateFileService;
