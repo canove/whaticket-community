2be81ab661209358dd6c7365d29af0be5bb37d8f
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2022-09-22 12:03:19 -0300

    Add Seed Create MenuLink to CompanyId=1

---
+++ backend/src/database/seeds/20200904070011-create-default-menus-company-relation.ts
@@ -0,0 +1,26 @@
+import { QueryInterface } from "sequelize";
+
+module.exports = {
+  up: async (queryInterface: QueryInterface) => {
+    const menu: any = await queryInterface.sequelize.query(
+      'SELECT id FROM whaticket.Menus WHERE name="Menu Link" and isParent=false;'
+    );
+
+    return await queryInterface.bulkInsert(
+      "MenuCompanies",
+      [
+        {
+          menuId: menu[0][0].id,
+          companyId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkDelete("MenuCompanies", {});
+  }
+};
