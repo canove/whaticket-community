7a20f1c09a4296a86c427acdea4f31e294f60d3a
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2022-12-14 10:27:46 -0300

    Update

--- backend/src/services/WbotServices/SendWhatsAppMedia.ts
+++ backend/src/services/WbotServices/SendWhatsAppMedia.ts
@@ -104,7 +104,12 @@ const SendWhatsAppMedia = async ({
         if (type != "image" && type != "audio" && type != "video" && type != "sticker") type = "document";
 
         if (type === "document") {
-          typePayload = { link, caption: media.originalname }
+          typePayload = { link, caption: media.originalname, filename: media.originalname }
+          // typePayload = {
+          //   link,
+          //   caption: `Mensagem \n Linha 1
+          //             Linha2`,
+          // filename: media.originalname }
         } else {
           typePayload = { link };
         }
