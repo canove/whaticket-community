9026a71724d7c5f65b844665eadf578485616eb6
root <root@vmi838493.contaboserver.net>
2023-01-24 15:32:46 +0100

    Server fulltimechat

--- frontend/server.js
+++ frontend/server.js
@@ -6,4 +6,4 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(3333);
+app.listen(3000);
