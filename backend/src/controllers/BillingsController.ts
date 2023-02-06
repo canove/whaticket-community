9594433ec0f61590534a0244e03a0e38861f97db
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2023-02-06 11:43:46 -0300

    Update Billings + Payments + Dashboard

--- backend/src/controllers/BillingsController.ts
+++ backend/src/controllers/BillingsController.ts
@@ -5,6 +5,12 @@ import ShowBillingsHistoricService from "../services/BillingServices/ShowBilling
 import ShowBillingsService from "../services/BillingServices/ShowBillingsService";
 
 import AppError from "../errors/AppError";
+import BillingControls from "../database/models/BillingControls";
+import { Sequelize } from "sequelize-typescript";
+import { Op } from "sequelize";
+import Product from "../database/models/Products";
+import Pricing from "../database/models/Pricing";
+import Billings from "../database/models/Billings";
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
   const { user } = req;
@@ -46,3 +52,91 @@ export const historic = async (
 
   return res.status(200).json(billings);
 };
+
+type DashboardQuery = {
+  selectedCompany: string;
+}
+
+export const dashboard = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { companyId } = req.user;
+  const { selectedCompany } = req.query as DashboardQuery;
+
+  const comp = selectedCompany ? selectedCompany : companyId
+
+  const { monthTotalValue, expectedTotalMonthValue } = await getMonthTotalValue(comp);
+  const lastMonthTotalValue = await getLastMonthTotalValue(comp);
+
+  return res.status(200).json({ monthTotalValue, expectedTotalMonthValue, lastMonthTotalValue });
+};
+
+const getLastMonthTotalValue = async (companyId: number | string) => {
+  const now = new Date();
+  const month = now.getMonth() === 0 ? 12 : now.getMonth();
+  const year = month === 12 ? now.getFullYear() - 1 : now.getFullYear();
+
+  const billing = await Billings.findOne({
+    where: { 
+      "$Billings.month$": Sequelize.where(Sequelize.fn("MONTH", Sequelize.col("Billings.createdAt")), month.toString()),
+      "$Billings.year$": Sequelize.where(Sequelize.fn("YEAR", Sequelize.col("Billings.createdAt")), year.toString()),
+      companyId
+    }
+  });
+
+  if (billing) {
+    const totalValue = billing.totalValue;
+
+    return totalValue;
+  }
+
+  return 0;
+}
+
+const getMonthTotalValue = async (companyId: number | string) => {
+  const now = new Date();
+  const today = now.getDate();
+  const month = now.getMonth() + 1;
+  const year = now.getFullYear();
+
+  const billingControls = await BillingControls.findAll({
+      where: { 
+          "$BillingControls.month$": Sequelize.where(Sequelize.fn("MONTH", Sequelize.col("BillingControls.createdAt")), month.toString()),
+          "$BillingControls.year$": Sequelize.where(Sequelize.fn("YEAR", Sequelize.col("BillingControls.createdAt")), year.toString()),
+          companyId
+      }
+  });
+
+  let triggerTotal = 0;
+
+  for (const billingControl of billingControls) {
+      const graceTriggers = billingControl.usedGraceTriggers;
+      const triggerFee = billingControl.triggerFee;
+      const quantity = billingControl.quantity;
+
+      triggerTotal = triggerTotal + (triggerFee * (quantity - graceTriggers));
+  }
+
+  const pricing = await Pricing.findOne({
+    where: { companyId },
+    include: [
+      {
+        model: Product,
+        as: "product",
+        attributes: ["monthlyFee"],
+        required: true
+      },
+    ]
+  });
+
+  if (!pricing) return { monthTotalValue: 0, expectedTotalMonthValue: 0 }
+  
+  const totalMonthValue = pricing.product.monthlyFee;
+  const totalTriggerValue = triggerTotal;
+  const monthTotalValue = totalMonthValue + totalTriggerValue;
+  
+  const expectedTotalMonthValue = ((totalTriggerValue / today) * 30) + totalMonthValue;
+
+  return { monthTotalValue, expectedTotalMonthValue };
+}
