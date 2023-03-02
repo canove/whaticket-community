8c6c7100eb26971e27c699ac0a932b5b597e33b9
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2023-03-02 11:48:14 -0300

    Update Send Whatsapp Message

--- backend/src/utils/common.ts
+++ backend/src/utils/common.ts
@@ -1,4 +1,6 @@
 /*eslint-disable */
+const AWS = require('aws-sdk');
+
 const preparePhoneNumber = (phone:string): string => {
   let phoneNumber = phone.replace('+','');
   let country = phoneNumber.substring(0,2);
@@ -107,4 +109,34 @@ const isValidHttpUrl = (text: string): any => {
   return null;
 }
 
-export { preparePhoneNumber, preparePhoneNumber9Digit, isValidHttpUrl, removePhoneNumber9Digit,removePhoneNumberCountry,removePhoneNumber9DigitCountry, removePhoneNumberWith9Country };
+const CONSTANT = {
+  region: process.env.ENV_AWS_REGION,
+  key:  process.env.ENV_AWS_ACCESS_KEY_ID,
+  secret: process.env.ENV_AWS_SECRET_ACCESS_KEY
+}
+
+const SQS = new AWS.SQS({
+  region: CONSTANT.region,
+  secretAccessKey:CONSTANT.secret,
+  accessKeyId: CONSTANT.key
+});	
+
+const sendMessageToSQS = async (params): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    SQS.sendMessage(params, function(err, data) {
+      if (err) console.error('SendWhatsAppMessage - Message could not be sent to SQS')
+  
+      console.log(' -- SendWhatsAppMessage - Message sent to SQS -- ', params.QueueUrl);
+      try {
+        setTimeout(function(){
+          resolve();
+        }, 1000);
+      } catch (e) {
+        console.log("SendWhatsAppMessage - Message Error", e);
+          resolve();
+      }
+    });
+  });
+}
+
+export { preparePhoneNumber, preparePhoneNumber9Digit, isValidHttpUrl, removePhoneNumber9Digit,removePhoneNumberCountry,removePhoneNumber9DigitCountry, removePhoneNumberWith9Country, sendMessageToSQS };
