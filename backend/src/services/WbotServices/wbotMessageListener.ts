d327b460db2578371436e9e521d39784e58d9fd0
allgood <claudemir@todobom.com>
2023-03-15 12:25:08 -0300

    * add random component to received filenames - fix #522

--- backend/src/services/WbotServices/wbotMessageListener.ts
+++ backend/src/services/WbotServices/wbotMessageListener.ts
@@ -63,6 +63,20 @@ const verifyQuotedMessage = async (
   return quotedMsg;
 };
 
+
+// generate random id string for file names, function got from: https://stackoverflow.com/a/1349426/1851801
+function makeRandomId(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 const verifyMediaMessage = async (
   msg: WbotMessage,
   ticket: Ticket,
@@ -76,9 +90,13 @@ const verifyMediaMessage = async (
     throw new Error("ERR_WAPP_DOWNLOAD_MEDIA");
   }
 
+  let randomId = makeRandomId(5);
+
   if (!media.filename) {
     const ext = media.mimetype.split("/")[1].split(";")[0];
-    media.filename = `${new Date().getTime()}.${ext}`;
+    media.filename = `${randomId}-${new Date().getTime()}.${ext}`;
+  } else {
+    media.filename = media.filename.split('.').slice(0,-1).join('.')+'.'+randomId+'.'+media.filename.split('.').slice(-1);
   }
 
   try {
