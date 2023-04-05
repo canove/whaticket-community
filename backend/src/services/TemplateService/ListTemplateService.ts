fe1f17280e01f1eb8bbd0bd47b79cd8455cc2f6d
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2023-04-05 10:05:27 -0300

    Whatsapp Official - Add function create new ticket

--- backend/src/services/TemplateService/ListTemplateService.ts
+++ backend/src/services/TemplateService/ListTemplateService.ts
@@ -4,19 +4,36 @@ import OfficialTemplates from "../../database/models/OfficialTemplates";
 import AppError from "../../errors/AppError";
 import ShowOfficialWhatsappService from "../OfficialWhatsappServices/ShowOfficialWhatsappService";
 import ShowWhatsAppService from "../WhatsappService/ShowWhatsAppService";
+import OfficialTemplatesStatus from "../../database/models/OfficialTemplatesStatus";
 
 interface Request {
   companyId: string | number;
+  whatsappId?: string;
 }
 
 const ListMetaTemplateService = async ({
-  companyId
+  companyId,
+  whatsappId
 }: Request): Promise<OfficialTemplates[]> => {
-    const templates = await OfficialTemplates.findAll({
-        where: { companyId, deletedAt: null }
-    });
+  let includeCondition = null;
 
-    return templates;
+  if (whatsappId) {
+    includeCondition = [
+      {
+        model: OfficialTemplatesStatus,
+        as: "officialTemplatesStatus",
+        where: { status: "APPROVED", whatsappId },
+        required: true
+      }
+    ];
+  }
+
+  const templates = await OfficialTemplates.findAll({
+    where: { companyId, deletedAt: null },
+    include: includeCondition
+  });
+
+  return templates;
 };
 
 export default ListMetaTemplateService;
