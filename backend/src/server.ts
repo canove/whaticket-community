7174cf5b1081f7e0e0381d8c150ef86c8b7b1c71
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2023-05-08 17:53:48 -0300

    Update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,9 @@ import { logger } from "./utils/logger";
 import { initConsumer } from "./utils/initConsumer";
 import { initBotMessageConsumer } from "./utils/initBotMessageConsumer";
 import { initMessageResponseConsumer } from "./utils/initMessageResponseConsumer";
+import { initMessageStatusConsumer } from "./utils/initMessageStatusConsumer";
 // import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
+const cron = require('node-cron');
 
 const server = app.listen(process.env.PORT, () => {
   logger.info(`Server started on port: ${process.env.PORT}`);
@@ -19,6 +21,23 @@ initBotMessageConsumer();
 console.log('\n\nWelcome to SQS BOT MESSAGE CONSUMER');
 initMessageResponseConsumer();
 console.log('\n\nWelcome to SQS MESSAGE RESPONSE CONSUMER');
-// StartAllWhatsAppsSessions();
+
+const MAX_CONSUMER_QUANTITY = 20;
+
+let consumerQuantity = 0;
+
+cron.schedule('* */1 * * * *', async () => {
+  try {
+    if (consumerQuantity < MAX_CONSUMER_QUANTITY) {
+      consumerQuantity += 1;
+      await initMessageStatusConsumer();
+      consumerQuantity -= 1;
+    }
+  } catch (err) {
+    consumerQuantity -= 1;
+  }
+},{ timezone: 'America/Sao_Paulo' });
+
+// !StartAllWhatsAppsSessions();
 
 gracefulShutdown(server);
