92b63a59228164330319955e0cd0f105af4a4fbc
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2023-05-16 11:08:40 -0300

    Update Supervisor: Can only see his own queues

--- backend/src/services/UserServices/ListAllUsersService.ts
+++ backend/src/services/UserServices/ListAllUsersService.ts
@@ -3,21 +3,35 @@ import Company from "../../database/models/Company";
 import Profiles from "../../database/models/Profiles";
 import Queue from "../../database/models/Queue";
 import User from "../../database/models/User";
+import ShowUserService from "./ShowUserService";
 
 interface Request {
   companyId: number;
+  loggedInUserId: string | number;
 }
 
 const ListAllUsersService = async ({
-  companyId
+  companyId,
+  loggedInUserId
 }: Request): Promise<User[]> => {
   let whereCondition = null;
+
+  const loggedInUser = await ShowUserService(loggedInUserId, companyId);
+  const userQueues = loggedInUser?.queues?.map((q) => q.id);;
 
   whereCondition = { companyId, deletedAt: null };
 
   const users = await User.findAll({
     where: whereCondition,
-    attributes: ["nickname", "name", "id", "useNickname"],
+    attributes: ["id", "name", "nickname", "useNickname"],
+    include: [
+      {
+        model: Queue,
+        as: "queues",
+        where: { id: userQueues },
+        required: true,
+      },
+    ],
     order: [["createdAt", "DESC"]],
   });
 
