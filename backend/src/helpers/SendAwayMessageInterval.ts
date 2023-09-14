0237cf522fe85ca785cdc9d124efda80aa572e0f
Loly Maluf <lolymaluf@gmail.com>
2023-09-14 14:53:58 -0300

    FEAT: Implementa Mensagem Ausencia no Back

---
+++ backend/src/helpers/SendAwayMessageInterval.ts
@@ -0,0 +1,25 @@
+
+import Ticket from "../models/Ticket";
+import SendWhatsAppMessage from "../services/WbotServices/SendWhatsAppMessage";
+
+export const SendAwayMessageInterval = async (
+  message: string,
+  ticket: Ticket,
+  timeInput: number
+) => {
+  const lastMessage = ticket.messages[-1];
+  const seconds = timeInput * 1000;
+  const timeoutId = setTimeout(() => {
+    if (ticket.status === "open" && lastMessage.fromMe && timeInput > 0) {
+      const now = new Date().getTime();
+      const sentAt = new Date(lastMessage.createdAt).getTime();
+      const timeElapsed = now - sentAt;
+      if (timeElapsed > seconds) {
+        SendWhatsAppMessage({ body: message, ticket });
+      }
+    }
+  }, seconds);
+  if (!lastMessage.fromMe) {
+    clearTimeout(timeoutId);
+  }
+};
