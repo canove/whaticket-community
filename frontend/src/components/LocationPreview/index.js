3cf3885066ea404b9c3eb29a6ad497a1a06d8eaa
Amanda Portela <mandaptela@gmail.com>
2023-10-27 22:26:51 -0300

    adição de alt na tag img

--- frontend/src/components/LocationPreview/index.js
+++ frontend/src/components/LocationPreview/index.js
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react';
 import toastError from "../../errors/toastError";
 
 import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
+//import Grid from "@material-ui/core/Grid";
 
 import { Button, Divider, } from "@material-ui/core";
 
 const LocationPreview = ({ image, link, description }) => {
-    useEffect(() => {}, [image, link, description]);
+    useEffect(() => {console.log(link);}, [image, link, description]);
 
     const handleLocation = async() => {
         try {
@@ -24,7 +24,7 @@ const LocationPreview = ({ image, link, description }) => {
 			}}>
 				<div>
 					<div style={{ float: "left" }}>
-						<img src={image} onClick={handleLocation} style={{ width: "100px" }} />
+						<img src={image} alt="map-image-location-preview" onClick={handleLocation} style={{ width: "100px" }} />
 					</div>
 					{ description && (
 					<div style={{ display: "flex", flexWrap: "wrap" }}>
