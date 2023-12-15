c305874a23432d2bf9bf691582c1b6415c039e10
Amanda Portela <mandaptela@gmail.com>
2023-12-14 21:35:19 -0300

    adição de "ticket" no conteúdo passado para o componente "MessageInput"

--- frontend/src/components/Ticket/index.js
+++ frontend/src/components/Ticket/index.js
@@ -169,7 +169,7 @@ const Ticket = () => {
             ticketId={ticketId}
             isGroup={ticket.isGroup}
           ></MessagesList>
-          <MessageInput ticketStatus={ticket.status} />
+          <MessageInput ticketStatus={ticket.status} ticket={ticket} />
         </ReplyMessageProvider>
       </Paper>
       <ContactDrawer
