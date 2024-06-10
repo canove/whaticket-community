7f0703c197e0eadcae39561effcbd97da3fa339d
Ruben Sedano <rubensedanoc@gmail.com>
2024-06-10 15:45:26 -0500

    cambios con nuevo servidor socket io

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -11,6 +11,10 @@ export function getBackendUrl() {
     return getConfig('REACT_APP_BACKEND_URL');
 }
 
+export function getNodeUrl() {
+    return getConfig('REACT_APP_NODE_URL');
+}
+
 export function getHoursCloseTicketsAuto() {
     return getConfig('REACT_APP_HOURS_CLOSE_TICKETS_AUTO');
 }
