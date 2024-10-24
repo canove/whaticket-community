74697079acbb00e17703f476bdb2e2cfd8a01e90
camiloLelis <lafirmafuerte@gmail.com>
2024-10-24 14:44:04 -0300

    feat: ajusta  para trazer o conteúdo da coluna imagePath

--- backend/src/services/UserServices/ShowUserService.ts
+++ backend/src/services/UserServices/ShowUserService.ts
@@ -5,7 +5,7 @@ import Whatsapp from "../../models/Whatsapp";
 
 const ShowUserService = async (id: string | number): Promise<User> => {
   const user = await User.findByPk(id, {
-    attributes: ["name", "id", "email", "profile", "tokenVersion", "whatsappId"],
+    attributes: ["name", "id", "email","imagePath", "profile", "tokenVersion", "whatsappId"],
     include: [
       { model: Queue, as: "queues", attributes: ["id", "name", "color"] },
       { model: Whatsapp, as: "whatsapp", attributes: ["id", "name"] },
