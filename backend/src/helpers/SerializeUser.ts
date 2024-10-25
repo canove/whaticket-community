03c4d83f0f2a024d136403ab9ad6934c3a34a9d0
camiloLelis <lafirmafuerte@gmail.com>
2024-10-25 00:35:30 -0300

    feat: return com imaPathatual

--- backend/src/helpers/SerializeUser.ts
+++ backend/src/helpers/SerializeUser.ts
@@ -9,6 +9,7 @@ interface SerializedUser {
   profile: string;
   queues: Queue[];
   whatsapp: Whatsapp;
+  imagePath: string;
 }
 
 export const SerializeUser = (user: User): SerializedUser => {
@@ -18,6 +19,7 @@ export const SerializeUser = (user: User): SerializedUser => {
     email: user.email,
     profile: user.profile,
     queues: user.queues,
-    whatsapp: user.whatsapp
+    whatsapp: user.whatsapp,
+    imagePath: user.imagePath
   };
 };
